ba634bd0a25e6eb9e3fca4f203dc30068c1de80c
Giulia Raffaella De Luca <101675809+giurdeluca@users.noreply.github.com>
2025-05-23 09:20:19 +0200

    fix import

--- colab_support/load_model.py
+++ colab_support/load_model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author  : chq_N
 # @Time    : 2020/8/29
+import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "" # Force CPU
 print("CUDA_VISIBLE_DEVICES set to:", os.environ.get("CUDA_VISIBLE_DEVICES"))
 import io
